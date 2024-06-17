d0c03c75424771ab7f403ac6cf8c291b71abff67
Vitaliy Pavlov <v-2841@yandex.ru>
2024-06-17 07:52:13 +0000

    add os check for icu

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from django.conf import settings
 
@@ -44,7 +45,7 @@ def get_client_ip(request):
 
 
 def load_icu(connection, **kwargs):
-    if connection.vendor != 'sqlite':
+    if connection.vendor != 'sqlite' and sys.platform != 'linux':
         return
     connection.connection.enable_load_extension(True)
     connection.connection.load_extension(
