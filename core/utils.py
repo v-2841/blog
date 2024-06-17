f8b8eee9829affd6ef58bb25250f97708d39721f
Vitaliy Pavlov <v-2841@yandex.ru>
2024-06-17 19:48:05 +0000

    add icu for Windows

--- core/utils.py
+++ core/utils.py
@@ -45,12 +45,6 @@ def get_client_ip(request):
 
 
 def load_icu(connection, **kwargs):
-    # if (connection.vendor != 'sqlite' or platform.system() != 'Linux'
-    #         or platform.machine() != 'x86_64'):
-    #     return
-    # connection.connection.enable_load_extension(True)
-    # connection.connection.load_extension(
-    #     os.path.join(settings.BASE_DIR, 'libSqliteIcu.so'))
     if connection.vendor != 'sqlite':
         return
     if platform.system() == 'Linux' and platform.machine() == 'x86_64':
